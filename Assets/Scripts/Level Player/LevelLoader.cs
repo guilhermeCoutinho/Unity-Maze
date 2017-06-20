730a2c19a358654da7bcd8044c19370ccd8a1242
GuilhermeCoutinho <guilhermejcoutinho@gmail.com>
2017-06-20 17:33:54 -0300

    Initial commit

---
+++ Assets/Scripts/Level Player/LevelLoader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour {
+
+    const string FILE_NAME = "Level ";
+    public int level;
+    public ObjectPool pool;
+
+    void Start ()
+    {
+        LoadLevel();
+    }
+
+    void LoadLevel ()
+    {
+        int colCount= 0;
+        List<int> data;
+        data = CSVParser.ParseCSV(FILE_NAME + level + ".csv", out colCount);
+
+       
+        int width = colCount;
+        int height = data.Count / colCount;
+
+        
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+               if (data[i * width + j] > 0)
+                {
+                    GameObject go = pool.getObject();
+                    go.transform.name = "" + data[i*width+j];
+                    go.transform.position = new Vector3( j -width/2, (height-1- i) -height/2 , 0);
+                }
+            }
+        }
+
+    }
+
+
+}
